8e0299eb12cf658189acc4029d91a6aa3a68a652
Oladayo Fagbemi <oofagbemi@outlook.com>
2019-09-11 00:06:00 +0200

    added basic functionality of router

--- Core/Router.php
+++ Core/Router.php
@@ -4,22 +4,48 @@
     class Router
     {
         protected $routes;
+        protected $params;
 
         public function __construct()
         {
             $this->routes = [];
+
         }
         /*
          * Adds new routes to the routes array
+         * @param string $route Route to be registered
+         * @param array $params Controller and corresponding action for the route
+         * @return void
          *
          * */
         public function add($route, $params)
         {
             $this->routes[$route] = $params;
         }
-
-        public function get()
+        /*
+         * Match the incoming url with the registered routes
+         * @param string url The route URL
+         * @return boolean true if route is found, false otherwise
+         * */
+        public function match($url)
         {
-        var_dump($this->routes);
+           foreach ($this->routes as $route=>$route_params)
+           {
+               if ($route === $url)
+               {
+                   $this->params = $route_params;
+                   return true;
+               }
+
+           }
+            return false;
+        }
+        /*
+         * gets the parameters of the currently matched route
+         * @return array
+         * */
+        public function getparams()
+        {
+        return $this->params;
         }
     }
--- public/index.php
+++ public/index.php
@@ -2,10 +2,20 @@
     require "../Core/Router.php";
     $router = new Router();
     $router->add('',['controller'=>'Home', 'action'=> 'index']);
-    $router->add('/posts',['controller'=>'Post', 'action'=> 'index']);
-    $router->add('/posts/create',['controller'=>'Post', 'action'=> 'create']);
+    $router->add('posts',['controller'=>'Post', 'action'=> 'index']);
+    $router->add('posts/create',['controller'=>'Post', 'action'=> 'create']);
 
-    $router->get();
+
+    $url = $_GET['url'];
+    $result = $router->match($url);
+    if($result) {
+        echo "there is a match" ."<br>";
+        var_dump($router->getparams());
+    } else {
+        echo "no match found for this url " . $url;
+    }
+
+
 
 
 
